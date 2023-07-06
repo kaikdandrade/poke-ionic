5f3a17e1cf592f28ef52057211dd99850e86789e
Kaik D' Andrade <kaikdossantossilva2@gmail.com>
2023-07-05 23:58:06 -0300

    Build Ionic

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  appName: 'poke-ionic',
-  credits: 'Kaik D\' Andrade',
-  creditsUrl: 'https://github.com/kaikdandrade',
-  apiURL: 'https://pokeapi.co/api/v2/'
-};
--- src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  appName: 'poke-ionic',
-  credits: 'Kaik D\' Andrade',
-  creditsUrl: 'https://github.com/kaikdandrade',
-  apiURL: 'https://pokeapi.co/api/v2/'
-};
